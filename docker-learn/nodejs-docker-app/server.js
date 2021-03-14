4f81d613c60ddacdb56b540a54cecbc8fb9f4100
Monkey_2G <ing4612@gmail.com>
2021-03-14 23:39:50 +0900

    2021.03.14 Docker-compose 작성

--- docker-learn/nodejs-docker-app/server.js
+++ docker-learn/nodejs-docker-app/server.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 // APP
 const app = express();
 app.get('/', (req,res) => {
-    res.send("Git fetch Test를 위한 문구 수정");
+    res.send("Git fetch Test를 위한 문구 수정123");
 });
 
 app.listen(PORT);
